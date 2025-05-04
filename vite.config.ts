d16e47854b2af7c19b959ec2ecd5edf7679a1796
xinyuguan3 <gxyguan55@gmail.com>
2025-05-04 17:15:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/benguan.work/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
