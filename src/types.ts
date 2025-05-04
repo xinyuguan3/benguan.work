0c0e5675f44ebde62f5343fd60a2355821cb62fe
xinyuguan3 <gxyguan55@gmail.com>
2025-05-04 16:32:39 +0800

    Updates

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface Book {
-  id: string;
-  title: string;
-  author: string;
-  description: string;
-  coverImage: string;
-  sideImage?: string;
-  category: 'Experience' | 'AI' | 'Tech' | 'Finance' | 'Sociology' ;
-}
