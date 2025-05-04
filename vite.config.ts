477db8eace0c61993a18ec2f1117b224151a831f
xinyuguan3 <gxyguan55@gmail.com>
2025-05-04 16:22:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/benguan.work/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
